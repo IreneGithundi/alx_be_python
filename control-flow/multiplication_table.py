0ef3b3604ce25de6f7a9bc3a28ff41fea9527da7
IreneGithundi <njokiirene4349@gmail.com>
2025-09-21 20:40:00 +0300

    Multiplication

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,5 @@
 number = int(input("Enter a number to see its multiplication table:"))
-multiplier = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for multiplier in multiplier:
+for multiplier in range (1, 11):
     result = multiplier * number
     print(f"{number} * {multiplier} = {result}")
