c7bde2841636b334cbc02186ecec465efa3878bf
IreneGithundi <njokiirene4349@gmail.com>
2025-09-21 22:15:59 +0300

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,32 +1,30 @@
-task = input("Describe your task: ")
-priority = input("What's the task priority? (high, medium, low) ")
-time = input("Is this task time_bound? (yes or no) ")
-yes = True
-no = False
+Task = input("Describe your task: ")
+Priority = input("What's the task priority? (high, medium, low) ")
+Time_Bound = input("Is this task time_bound? (yes or no) ")
 
-match priority:
+match Priority:
     case "high":
-        if time == "yes":
-            print(f"'{task}' is a {priority} priority task that requires immediate attention today!")
-        elif time == "no":
-            print(f"'{task}' is a {priority} task and does not need immediate attention")
+        if Time_Bound== "yes":
+            print(f"'{Task}' is a {Priority} priority task that requires immediate attention today!")
+        elif Time_Bound == "no":
+            print(f"'{Task}' is a {Priority} task and does not need immediate attention")
         else:
-            print(f"Does not recognize {time} as a time_bound")
+            print(f"Does not recognize {Time_Bound} as a time_bound")
     case "medium":
-        if time == "yes":
-            print(f"'{task}' is a {priority} priority task that requires attention today but not immediate!")
-        elif time == "no":
-            print(f"'{task}' is a {priority} task and does not need attention at the moment")
+        if Time_Bound == "yes":
+            print(f"'{Task}' is a {Priority} priority task that requires attention today but not immediate!")
+        elif Time_Bound == "no":
+            print(f"'{Task}' is a {Priority} task and does not need attention at the moment")
         else:
-            print(f"Does not recognize {time} as a time_bound")
+            print(f"Does not recognize {Time_Bound} as a time_bound")
     case "low":
-        if time == "yes":
-            print(f"'{task}' is a {priority} priority task that requires attention some other day!")
-        elif time == "no":
-            print(f"'{task}' is a {priority} task and does not need attention")
+        if Time_Bound == "yes":
+            print(f"'{Task}' is a {Priority} priority task that requires attention some other day!")
+        elif Time_Bound == "no":
+            print(f"'{Task}' is a {Priority} task and does not need attention")
         else:
-            print(f"Does not recognize {time} as a time_bound")
+            print(f"Does not recognize {Time_Bound} as a time_bound")
     case _:
-        print(f"Does not recognize {priority} as required input")
+        print(f"Does not recognize {Priority} as required input")
 
 
