722c1aef74ebe03a0ac34cf3449a285c9126160b
IreneGithundi <njokiirene4349@gmail.com>
2025-10-17 22:21:39 +0300

    Bank Account Balances

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,7 +14,7 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print(f"Current Balance: ${self.acc_balance}")
+        print(f"Current Balance: ${self.acc_balance:.2f}")
 
 
 
