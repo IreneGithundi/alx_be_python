32c26848414e73e784e82fe0a7f2cd386200f3ed
IreneGithundi <njokiirene4349@gmail.com>
2025-10-21 00:09:06 +0300

    Bank Account Balances

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,23 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.acc_balance = account_balance
-        account_balance = 0
-    
+        if account_balance >= 0:
+            self._account_balance = account_balance
+
+    @property
+    def account_balance(self):
+        return self._account_balance
+
     def deposit(self, amount):
-        self.acc_balance += amount
+        if amount > 0:
+            self._account_balance += amount
 
     def withdraw(self, amount):
-        if self.acc_balance >= amount:
-            self.acc_balance -= amount
+        if amount < self._account_balance:
+            self._account_balance -= amount
             return True
         else:
             return False
         
     def display_balance(self):
-        print(f"Current Balance: ${self.acc_balance:.2f}")
-
-
+        print(f"Current Balance: ${self._account_balance}")
 
